e6ac67a7dbd50443a4d83443387dd4c8f5a74d62
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-07-02 23:11:56 +0600

    Fix index error

--- reverse_string.go
+++ reverse_string.go
@@ -3,7 +3,7 @@ package reverse_string
 func ReverseString(input string) (output string) {
 	// solution goes here
 	runes := []rune(input)
-	for i := len(input) - 1; i >= 0; i-- {
+	for i := len(runes) - 1; i >= 0; i-- {
 		output += string(runes[i])
 	}
 	return output
